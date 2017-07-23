c2ba9786a4b1479c1d92d447d237663e54774aa5
Dan Cusher <dan@dancusher.com>
2017-07-23 01:29:22 -0400

    Make panel footers random colors on hover

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,15 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+$( document ).ready(function() {
+  $('.mypanel').hover(function() {
+    $(this).find('.panel-footer').css(
+      'background-color',
+      '#' + Math.floor(Math.random() * 16777215).toString(16)
+    );
+  }, function() {
+    $(this).find('.panel-footer').css('background-color', '');
+  });
+});
+
