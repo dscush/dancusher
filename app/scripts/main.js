a89b1042a2f99b66ef304bc26e2bdac2d16c1479
Dan Cusher <dan@dancusher.com>
2017-07-27 00:53:00 -0400

    Improve versatility of random colors

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,11 +2,16 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
 
+function rnd_color_level(min = 0, max = 16) {
+  return "00".replace(/0/g,function(){return (~~(Math.random() * (max - min) + min)).toString(16);});
+}
+
 $( document ).ready(function() {
   $('.mypanel').hover(function() {
+    let color = '#' + rnd_color_level(0,8) + "FF" + rnd_color_level(8);
     $(this).find('.panel-footer').css(
       'background-color',
-      '#' + Math.floor(Math.random() * 16777215).toString(16)
+      color
     );
   }, function() {
     $(this).find('.panel-footer').css('background-color', '');
