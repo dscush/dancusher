8e7aee4a44ee71ecdbf635eb3d74b10d35ed80db
Dan Cusher <dan@dancusher.com>
2017-07-27 01:34:09 -0400

    Further improve versatility of random colors

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,11 +8,19 @@ function rnd_color_level(min = 0, max = 16) {
 
 $( document ).ready(function() {
   $('.mypanel').hover(function() {
-    let color = '#' + rnd_color_level(0,8) + "FF" + rnd_color_level(8);
+    let colors = [
+      rnd_color_level(0,8),
+      rnd_color_level(0,8),
+      rnd_color_level(14)
+    ]
+    let first_color = Math.floor(Math.random() * 3);
+    let color = '#' + colors[first_color] + colors[(first_color + 1) % 3] + colors[(first_color + 2) % 3];
     $(this).find('.panel-footer').css(
       'background-color',
       color
     );
+    $('strong').css('color', color);
+    $('p > a').css('color', color);
   }, function() {
     $(this).find('.panel-footer').css('background-color', '');
   });
